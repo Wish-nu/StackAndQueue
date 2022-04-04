6a8bf3ce2d8f9960cfa09b59dd6781bc1a1ca079
Vishnu Prasad Preetham Kumar <81408708+Wish-nu@users.noreply.github.com>
2022-04-04 23:53:27 +0530

    Update StackImplementation.java

--- StackImplementation.java
+++ StackImplementation.java
@@ -1,7 +1,7 @@
 package com.gl.demo;
 
 class StackImplementation {
-    static final int Size_of_stack = 1000;
+    static final int Size_of_stack = 100;      // can hold 100 elements
     int current_pointer ;
     int a[] = new int[Size_of_stack]; // Maximum size of Stack
     
@@ -10,23 +10,23 @@ class StackImplementation {
     }
     
     boolean push(int x){
-        if (current_pointer >= (Size_of_stack - 1)) {
+        if (current_pointer >= (Size_of_stack - 1)) {       //when pointer at the end of the array, its overflow
             System.out.println("Stack Overflow");
             return false;
             }
         else {
-            a[++current_pointer ] = x;
+            a[++current_pointer ] = x;                  // else keeping assigning for current pointer till condition false
             System.out.println(x + " pushed into stack");
             return true;
             }
             }
-    int pop(){
+    int pop(){                                      // pop method 
         if (current_pointer < 0) {
-         System.out.println("Stack Underflow");
+         System.out.println("Stack Underflow");     //cause pointer is less than 0
          return 0;
          }
         else {
-        int x = a[current_pointer--]; 
+        int x = a[current_pointer--];               //take the value of current pointer and store in x and keep poping
         return x;
         }
         }
